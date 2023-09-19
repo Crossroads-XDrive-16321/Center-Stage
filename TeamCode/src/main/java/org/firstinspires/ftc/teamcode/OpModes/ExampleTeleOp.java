c2f045561fe3d9c4705ee149c94c45461e3099ab
splitpea89 <benashaheen@icloud.com>
2023-09-19 15:29:11 -0700

    example tele op with tank drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/ExampleTeleOp.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class ExampleTeleOp extends LinearOpMode {
+
+    DcMotorEx frontLeft, frontRight, backLeft, backRight;
+
+    void initialize() {
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize();
+
+        while(!isStopRequested()) {
+
+            frontLeft.setPower(-gamepad1.left_stick_y);
+            backLeft.setPower(-gamepad1.left_stick_y);
+            frontRight.setPower(-gamepad1.right_stick_y);
+            backRight.setPower(-gamepad1.right_stick_y);
+
+        }
+        
+    }
+
+}
