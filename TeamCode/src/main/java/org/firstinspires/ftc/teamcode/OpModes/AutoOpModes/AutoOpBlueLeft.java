d2a1c15ff223190059e2b97a31c78d73e19da653
Best25x <xBest25x@gmail.com>
2023-11-04 13:39:27 -0700

    start of auto modes

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AutoOpModes/AutoOpBlueLeft.java
@@ -0,0 +1,59 @@
+package org.firstinspires.ftc.teamcode.OpModes.AutoOpModes;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.Helpers.ClawController;
+import org.firstinspires.ftc.teamcode.Helpers.DriveController;
+
+@Autonomous
+public class AutoOpBlueLeft extends LinearOpMode {
+
+    DcMotorEx frontLeft, frontRight, backLeft, backRight, slideRotatorLeft, slideRotatorRight, slideMotor;
+    DriveController driveController;
+
+    Servo leftClaw, rightClaw, clawServo, planeLauncher;
+    ClawController clawController;
+
+    void initialize() {
+        frontLeft = hardwareMap.get(DcMotorEx.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotorEx.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotorEx.class, "backLeft");
+        backRight = hardwareMap.get(DcMotorEx.class, "backRight");
+        slideRotatorLeft = hardwareMap.get(DcMotorEx.class, "slideRotatorLeft");
+        slideRotatorRight = hardwareMap.get(DcMotorEx.class, "slideRotatorRight");
+        slideMotor = hardwareMap.get(DcMotorEx.class, "slideMotor");
+
+        driveController = new DriveController(frontLeft, backLeft, frontRight, backRight, slideRotatorLeft, slideRotatorRight, slideMotor);
+        driveController.init();
+
+        planeLauncher = hardwareMap.get(Servo.class, "planeLauncher");
+
+        leftClaw = hardwareMap.get(Servo.class, "leftClaw");
+        rightClaw = hardwareMap.get(Servo.class, "rightClaw");
+        clawServo = hardwareMap.get(Servo.class, "clawServo");
+
+        clawController = new ClawController(leftClaw, rightClaw, clawServo);
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        initialize();
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+
+            //parking
+                //move forward 1inch
+
+                //move left short dist
+        }
+
+    }
+
+
+}
