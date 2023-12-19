b291b52f2e47c0f7a8665f8ae1a61faa5a8c354a
Best25x <xBest25x@gmail.com>
2023-12-18 20:14:59 -0800

    started hard coding auto instructions with roadrunner
    skipped the entire calibration step cause im not home so im just doing what i can "asynchronously"

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,16 +14,19 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15) //TODO: change these values once we finish the constraints
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0,0, Math.toRadians(0)))
-                                .forward(24)
-                                .turn(Math.toRadians(90))
-                                .forward(24)
-                                .turn(Math.toRadians(-90))
-                                .forward(24)
-                                .turn(Math.toRadians(90))
-                                .forward(24)
-                                .turn(Math.toRadians(90))
-                                .splineToSplineHeading(new Pose2d(-24,48,Math.toRadians(42)), Math.toRadians(42))
+                        drive.trajectorySequenceBuilder(new Pose2d(14,61, Math.toRadians(270)))
+                                .splineToLinearHeading(new Pose2d(34,32, Math.toRadians(180)),Math.toRadians(180))
+                                .waitSeconds(2)//drop purple
+                                .lineToConstantHeading(new Vector2d(48, 29))
+                                .waitSeconds(2)//drop yellow
+                                .splineToLinearHeading(new Pose2d(24,11,Math.toRadians(180)),Math.toRadians(180))
+                                .lineToLinearHeading(new Pose2d(-56,11,Math.toRadians(180)))
+                                .waitSeconds(2)//pickup two white
+                                .lineToLinearHeading(new Pose2d(24,11,Math.toRadians(180)))
+                                .splineToConstantHeading(new Vector2d(48,36), Math.toRadians(0))
+                                .waitSeconds(2)//drop two white
+                                .lineToConstantHeading(new Vector2d(48,12))
+                                .forward(-12)
                                 .build()
                 );
 
