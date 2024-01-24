d7fad3fc54093bec3b99b0a0cfbb354f8d6edb3c
Best25x <xBest25x@gmail.com>
2024-01-24 09:21:24 -0800

    added quick apriltag detection op mode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/AprilTagTesting.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.OpModes;
+
+import android.graphics.Canvas;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
+import org.opencv.core.Mat;
+
+import java.util.ArrayList;
+
+public class AprilTagTesting extends LinearOpMode{
+
+    public void runOpMode() {
+
+        AprilTagProcessor tagProcessor = new AprilTagProcessor.Builder()
+
+                .build();
+
+        VisionPortal visionPortal = new VisionPortal.Builder()
+                .addProcessor(tagProcessor)
+                .setCamera(hardwareMap.get(WebcamName.class, "Webcam 1"))
+                .build();
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+            if (tagProcessor.getDetections().size() > 0) {
+                AprilTagDetection tag = tagProcessor.getDetections().get(0);
+
+                telemetry.addData("x",tag.ftcPose.x);
+                telemetry.addData("y",tag.ftcPose.y);
+                telemetry.addData("z",tag.ftcPose.z);
+                telemetry.addData("pitch",tag.ftcPose.pitch);
+                telemetry.addData("yaw",tag.ftcPose.yaw);
+                telemetry.addData("roll",tag.ftcPose.roll);
+
+            }
+        }
+    }
+}
